5fef9d1319df7028418910cfc82c55954b5282e0
Abdul Malek <abdulmalek67343367@gmail.com>
2025-11-25 15:49:06 +0600

    Update page.js
     add gradiant in my footer

--- app/footer/page.js
+++ app/footer/page.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-300 text-black-900 font-bold py-10">
+    <footer className=""bg-gradient-to-r from-blue-500 to-red-600 text-black-900 font-bold py-10">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
 
         {/* Logo + Description */}
