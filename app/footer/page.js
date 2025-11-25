91a66e7092422e1d1f5e9900d32668e9677e6a25
Abdul Malek <abdulmalek67343367@gmail.com>
2025-11-25 15:27:04 +0600

    Update page.js
    changes footer background color

--- app/footer/page.js
+++ app/footer/page.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-300 py-10">
+    <footer className="bg-gray-300 text-white py-10">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
 
         {/* Logo + Description */}
