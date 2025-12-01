448936658678b03c640209fae909d79dff09737e
Abdul Malek <abdulmalek67343367@gmail.com>
2025-12-01 16:29:05 +0600

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -73,7 +73,7 @@ export default function Contact() {
         </button>
       </form>
 
-      <div className="mt-12 pt-8 border-t border-gray-200 text-center text-gray-500">
+      <div className="mt-12 pt-8 border-t border-gray-200 text-center text-white">
         <p>Or email me at: <span className="text-white-600 font-bold">abdulmalek67343367@gmail.com</span></p>
       </div>
 
